e85d93bc166ec99394d6c92d09e726ade4b6c681
crazyblue7 <visimonmora@gmail.com>
2024-12-12 07:54:26 -0300

    Add files via upload

---
+++ canvasCodeTest.js
@@ -0,0 +1,15 @@
+// functions/variables/consts?
+const el=document.getElementById('canvas');
+const pen=el.getContext('2d');
+var pi = Math.PI;
+
+/*function draw(){
+    console.log(time.time)
+}*/
+pen.beginPath();
+pen.moveTo(318, 347);
+pen.lineTo(337, 390);
+pen.stroke();
+
+pen.beginPath();
+pen.arc(640, 360, 0)
